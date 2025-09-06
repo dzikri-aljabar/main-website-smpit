09dd3172ea799a2a384a60e0ff04f77674057af5
unknown <dzikrialjabar06@gmail.com>
2025-09-06 19:52:33 +0700

    rename Home.blade.php → home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -166,43 +166,4 @@
                 </div>
             </div>
     </section>
-    {{-- <section class="mt-8">
-            <h1 class="text-center font-bold text-xl">Galeri</h1>
-            <div class="px-[20px] lg:px-[85px] mt-5">
-                <div class="grid gap-4">
-                    <div>
-                        <img class="h-auto max-w-full rounded-lg"
-                            src="https://flowbite.s3.amazonaws.com/docs/gallery/featured/image.jpg" alt="">
-                    </div>
-                    <div class="grid grid-cols-5 gap-4">
-                        <div>
-                            <img class="h-auto max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-                                alt="">
-                        </div>
-                        <div>
-                            <img class="h-auto max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
-                                alt="">
-                        </div>
-                        <div>
-                            <img class="h-auto max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
-                                alt="">
-                        </div>
-                        <div>
-                            <img class="h-auto max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
-                                alt="">
-                        </div>
-                        <div>
-                            <img class="h-auto max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
-                                alt="">
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </section> --}}
 </x-layouts.app>
