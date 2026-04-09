6270b8462f52612f6b53d7a40977003d03fa2382
dzikri-aljabar <dzikrialjabar06@gmail.com>
2026-04-09 10:21:08 +0700

    add commit

--- resources/views/berita.blade.php
+++ resources/views/berita.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.app>
     <section class="flex flex-row py-5 px-[20px] gap-x-7">
         <div class="basis-full md:basis-2/3">
-            <form class="max-w-9/10 mx-auto">
+            <form class="max-w-9/10 mx-auto" method="">
                 <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
                 <div class="relative">
                     <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -11,7 +11,7 @@
                                 d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                         </svg>
                     </div>
-                    <input type="search" id="default-search"
+                    <input  type="search" id="default-search"
                         class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-[#FF5700] focus:border-[#FF5700]"
                         placeholder="Cari berita terkini..." required />
                     <button type="submit"
