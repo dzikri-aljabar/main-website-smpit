4003fc6ab31773700e3eecfe8e4be40dd54c314f
dzikri-aljabar <dzikrialjabar06@gmail.com>
2026-04-14 10:08:01 +0700

    add: filament table view

--- app/Filament/Resources/Galeris/Tables/GalerisTable.php
+++ app/Filament/Resources/Galeris/Tables/GalerisTable.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\Galeris\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class GalerisTable
@@ -13,7 +15,8 @@ class GalerisTable
     {
         return $table
             ->columns([
-                //
+                ImageColumn::make('path')->label('Gambar Galeri')->visibility('public')->imageWidth(200)->imageHeight(100),
+                TextColumn::make('jenis')->label('Jenis Galeri'),
             ])
             ->filters([
                 //
